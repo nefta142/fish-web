e82a6595851ec8bc7687bf850942d36d92284bb4
neftali <neftaliespinorodriguez@gmail.com>
2025-12-01 17:23:37 +0000

    Mejora del js login

--- js/user-login.js
+++ js/user-login.js
@@ -3,7 +3,7 @@ let users = [];
 function showUsers() {
   let aux = "";
   for (let i = 0; i < users.length; i++) {
-    aux += `<li>${users[i].userName} - ${users[i].userEmail}</li>`;
+    aux += `<li>${users[i].userName} - ${users[i].userSurname} - ${users[i].userEmail} - ${users[i].userPhone}</li>`;
   }
   let usersList = document.getElementById("users-list");
   if (usersList) {
@@ -20,11 +20,15 @@ function addUserToArray(event) {
   event.preventDefault();
 
   let userName = event.target["user-name"].value;
+  let userSurname = event.target["user-surname"].value;
   let userEmail = event.target["user-email"].value;
+  let userPhone = event.target["user-phone"].value;
 
   let newUser = {
     userName: userName,
-    userEmail: userEmail
+    userSurname: userSurname,
+    userEmail: userEmail,
+    userPhone: userPhone
   };
 
   users.push(newUser);
